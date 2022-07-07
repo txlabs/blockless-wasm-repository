d320a19698699d1b68931b97bc84356ad67b4f0a
Derek Anderson <derek@akash.network>
2022-07-06 23:59:25 -0500

    use process.env

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import { Web3Storage } from "web3.storage";
 import dotenv from "dotenv";
 import { start as serverStart } from "./server";
 
-const envconfig: any = dotenv.config().parsed;
+dotenv.config();
 function getAccessToken() {
   // If you're just testing, you can paste in a token
   // and uncomment the following line:
@@ -12,7 +12,7 @@ function getAccessToken() {
   // environement variable or other configuration that's kept outside of
   // your code base. For this to work, you need to set the
   // WEB3STORAGE_TOKEN environment variable before you run your code.
-  return envconfig.WEB3STORAGE_TOKEN;
+  return process.env.WEB3STORAGE_TOKEN;
 }
 
 function makeStorageClient() {
@@ -20,4 +20,4 @@ function makeStorageClient() {
 }
 
 const storageClient = makeStorageClient();
-serverStart(storageClient, envconfig, 3005);
+serverStart(storageClient, process.env, 3005);
