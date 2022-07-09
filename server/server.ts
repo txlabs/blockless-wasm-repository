808511ba1834e4856b3bd7ed8dad58aeb8430571
Derek Anderson <derek@akash.network>
2022-07-09 01:34:20 -0500

    api for networking connect

--- server/server.ts
+++ server/server.ts
@@ -2,8 +2,22 @@ import Fastify from "fastify";
 import { File } from "web3.storage";
 import mongoose from "mongoose";
 import path from "path";
+import LRU from "lru-cache";
+import fileUpload from "fastify-file-upload";
 
-const fileUpload = require("fastify-file-upload");
+const options = {
+  sizeCalculation: (value: any, key: any) => {
+    return 1;
+  },
+  max: 500,
+  maxSize: 5000,
+  ttl: 1000 * 60 * 5,
+  allowStale: false,
+  updateAgeOnGet: false,
+  updateAgeOnHas: false,
+};
+
+const cache = new LRU(options);
 
 const fastify = Fastify({
   logger: true,
@@ -38,7 +52,7 @@ export const start = async (
 
   fastify.get("/api/list", async (request, reply) => {
     const modules = await Module.find();
-    reply.send(JSON.stringify(modules));
+    reply.send(JSON.stringify(modules, null, 2));
   });
 
   fastify.post("/api/submit", async (request: any, reply) => {
@@ -78,6 +92,80 @@ export const start = async (
 
     // send the user back the file info whenever
     reply.send(meta);
+  });
+
+  fastify.get("/api/sync", async (request, reply) => {
+    const modules = await Module.find();
+
+    const list = modules.map((module: any) => {
+      return `<a href="${module.cid}/">${module.cid}</a>`;
+    });
+
+    const html = `
+        <html>
+        <head><title>Index of /public-repository/</title></head>
+        <body>
+        <h1>Index of /public-repository/</h1><hr><pre><a href="../">../</a>
+        ${list}
+        </pre><hr></body>
+        </html>
+    `;
+    reply.header("Content-Type", "text/html").status(200).send(html);
+  });
+
+  fastify.get("/api/sync/:cid/:file", async (request, reply) => {
+    // fetch the file from the storage client
+    const { cid }: any = request.params;
+    const cacheid = request.url;
+    const cached = cache.get(cacheid); // "value"
+    const filePart = request.url.split("/").pop();
+
+    if (cached) {
+      fastify.log.info(`Cache hit for ${cid}`);
+      reply
+        .header("Content-Type", cached.type)
+        .status(200)
+        .send(Buffer.concat(cached.chunks));
+      return;
+    }
+
+    fastify.log.info(`Starting Upstream Request for ${cid}/${filePart}`);
+    const res: any = await fetch(`https://${cid}.ipfs.dweb.link/${filePart}`);
+    fastify.log.info(`Finished Upstream Request for ${cid}/${filePart}.json`);
+
+    const chunks = [];
+    for await (let chunk of res.body) {
+      chunks.push(chunk);
+    }
+
+    if (request.url.indexOf("manifest.json") > -1) {
+      // manifest
+      const manifest = JSON.parse(Buffer.concat(chunks).toString("utf-8"));
+      manifest.id = cid;
+
+      // cast back as chunks
+      const Readable = require("stream").Readable;
+      const s = new Readable();
+      s._read = () => {}; // redundant? see update below
+      s.push(JSON.stringify(manifest, null, 2));
+      s.push(null);
+      const _chunks = [];
+      for await (let chunk of s) {
+        _chunks.push(chunk);
+      }
+      // store chunks
+      cache.set(cacheid, { chunks: _chunks, type: "application/json" });
+
+      // serve
+      reply.type("application/json");
+      reply.send(JSON.stringify(manifest, null, 2));
+      return;
+    }
+
+    const returnType = res.headers.get("content-type");
+    reply.type(returnType);
+    cache.set(cacheid, { chunks, type: returnType });
+    reply.send(Buffer.concat(chunks));
   });
 
   fastify.setNotFoundHandler((request: any, reply: any) => {
